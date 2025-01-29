ff0528204b6bd4385aeaaab336cfde866deda399
auliarizki10 <4uli4.r101007@gmail.com>
2025-01-29 14:46:09 +0700

    menambahkan edit data kategori

--- kategori.php
+++ kategori.php
@@ -120,3 +120,31 @@ include '.includes/toast_notification.php';
            </div>
        </div>
     </div>
+
+    <!--modal untuk update data kategori --> 
+    <div id="editCategory_<?= $category['category_id']; ?>" class="modal fade" tabindex="-1" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title">Update Data Kategori</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+           </div>
+           <div class="modal-body">
+            <form action="proses_kategori.php" method="POST">
+              <!-- input tersembunyi untuk menyimpan id kategori -->
+               <input type="hidden" name="catID" value="<?= $category['category_id']; ?>">
+               <div class="form-group">
+                <label>Nama kategori</label>
+                <!-- input untuk nama kategori -->
+                 <input type="text" value= "<?= $category['category_name']; ?>" name=category_name class="form-control">
+               </div>
+               <div class= "modal-footer">
+                <button type="button" class= " btn btn-secondary" data-bs-dismiss="modal">Batal
+                </button>
+                <button type="submit" name="update" class="btn btn-warning">update</button>
+               </div>
+            </form>
+           </div>
+        </div>
+     </div>
+ </div>
